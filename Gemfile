61ea5853a4d8780c0783f0dc5cff5f735b03c9db
Adriaan Beiertz <adriaanbd@gmail.com>
2019-04-23 15:53:02 -0500

    Adding secure password with bcrypt dependency

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.6.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+#Use ActiveModel has_secure_password
+gem 'bcrypt'
 # Use sqlite3 as the database for Active Record
 group :development, :test do
   gem 'sqlite3' , '~> 1.3.13'
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_192742) do
+ActiveRecord::Schema.define(version: 2019_04_23_202957) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
