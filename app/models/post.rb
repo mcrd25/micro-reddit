3257d63beadbca2bf9c40198b442856d4f6e3b44
Adriaan Beiertz <adriaanbd@gmail.com>
2019-04-24 13:10:06 -0500

    Implemented Comment model with user post association and small validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true,
                     length: { maximum: 100, minimum: 4 }
   validates :body, presence: true,
